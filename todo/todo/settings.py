7ca22738d2396f3807eb95c46b4465f4ea65943d
Sergey <sergey.bukov@yandex.ru>
2022-02-21 06:11:52 +0300

    django_filters; pagination

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'userapp',
     'corsheaders',
+    'django_filters',
     'todoapp',
 ]
 
@@ -145,6 +146,9 @@ REST_FRAMEWORK = {
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
         # Any other parsers
     ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
 }
 
 # CORS
